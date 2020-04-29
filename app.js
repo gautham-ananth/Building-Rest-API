fcdbc2d840369c55f6ceec1e1e4dcb1f3bf2ee8f
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-04-29 20:05:03 +0530

    Codes added in app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 var app = express();
 
+//Middleware
+app.use('/posts', (req, res) => {
+    console.log("Middleware running");
+    res.send("Success");
+})
+
 //Routes
 app.get("/", (req, res) => {
     res.send("Response from the server will be displayed in the browser");
 })
+
 
 //Listen to the server
 app.listen(3000);
