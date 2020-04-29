f25d3d8ad5a776a1473e4576a94a718b413ba6fc
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-04-29 21:28:17 +0530

    schema defined in models/post.js

---
+++ models/post.js
@@ -0,0 +1,20 @@
+//This file defines the database schemea
+const mongoose = require('mongoose');
+
+var postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+//this maps the schemaname to the collection in mlab
+module.exports('Posts', postSchema);
