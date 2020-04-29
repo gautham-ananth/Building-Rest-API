a6577071dda748587815b49a053b1e3620b520e4
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-04-29 21:38:56 +0530

    body parser included whic parses the req.body

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
-
+app.use(bodyParser.json())
 
 //Routes in routes folder
 //Import Routes
